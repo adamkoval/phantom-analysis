a7a8c535867be8e35556c4780f6e9ec6f18315aa
Adam Koval <adam_koval@hotmail.com>
2023-11-02 17:19:39 +0000

    Added some commandline/config file functionality to the scripts. script_utils.py now includes the appropriate functions, SARRACEN_temp_density_plot.py can now use them appropriately.

--- SARRACEN_temp_density_plot.py
+++ SARRACEN_temp_density_plot.py
@@ -5,6 +5,27 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
+import script_utils as su
+
+# HOUSEKEEPING: Command line arguments or config options are processed here
+cmd_args = [['--config_file', '-c', 'store', 'config_file', 'Use config file instead of cmd input?'],
+		['--data_dir', '-d', 'store', 'data_dir', 'The directory with results'],
+		['--output_dir', '-o', 'store', 'output_dir', 'Where to store analysis output'],
+		['--store_data', '-sd', 'store_true', 'store_data', 'Whether to save the data into output'],
+		['--store_plot', '-sp', 'store_true', 'store_plot', 'Whether to save the plot into output'],
+		['--N_densest', '-N', 'store', 'N_densest', 'Number of densest particles to average ovrer']]
+_description = "A script to plot temp against density from PHANTOM SPH simulations using the SARRACEN package."
+
+config_file, data_dir, output_dir, store_data, store_plot = su.get_args(cmd_args)
+
+if config_file:
+	data_dir, output_dir, store_data, store_plot = su.get_config(config_file)
+
+if store_data==True or store_plot==True and not output_dir:
+	output_dir = input("No output path provided. Please provide an output path if you wish to save data OR plot (abs path to directory required):")
+
+
+# DATA PROCESSING: The interesting stuff
 def get_densest(sdf, N):
 	"""
 	sdf - dataframe with sim data
@@ -13,12 +34,9 @@ def get_densest(sdf, N):
 	return sdf.sort_values(by='rho', ascending=False).head(N)
 
 print("reading in data")
-data_dir = "/home/dc-kova1/scratch/programs/phantom_runs/tests/collapse_test_Lombardi/"
-fins_all = np.sort([file for file in os.listdir(data_dir) if re.match('colltest_[0-9].', file)])
+fins_all = np.sort([file for file in os.listdir(data_dir) if re.match('colltest_[0-9].', file)]) # Sort files by asc. timestep
 fins = fins_all
-#fins = ["colltest_00300"]
 
-N = 200
 temps_max = []
 rhos_max = []
 temps_avg = []
@@ -26,14 +44,14 @@ rhos_avg = []
 for fin in fins:
 	print("Working on file {}".format(fin))
 	fin = data_dir + fin
-	sdf = sarracen.read_phantom(fin)
-	sdf.calc_density()
+	sdf = sarracen.read_phantom(fin) # Read in using Sarracen
+	sdf.calc_density() # Calculate density using Sarracen
 
 	# Append to lists
 	temps_max.append(np.max(sdf['temperature']))
 	rhos_max.append(np.max(sdf['rho']))
 
-	densest = get_densest(sdf, N)
+	densest = get_densest(sdf, N_densest)
 	temps_avg.append(np.mean(densest['temperature']))
 	rhos_avg.append(np.mean(densest['rho']))
 
@@ -44,4 +62,5 @@ ax.loglog(rhos_avg, temps_avg, label='means')
 ax.legend()
 ax.set_xlabel('Density [a.u.]')
 ax.set_ylabel('Temperature [a.u.]')
+# [ADD SAVEFIG THING - check if I can add a timestamp from the data..]
 plt.show()
--- script_utils.py
+++ script_utils.py
@@ -1,23 +1,45 @@
 #
 # Utils for package managment
 #
+from argparse import ArgumentParser
 
-def get_args(cmd_args):
-	"""Gets some defined command line args. Should be modular, let's see how it works.
-	In:
-		> cmd_args (dct) - dict of args with mandatory
-		columns "action", "dest", "help", "required", in that order.
-	Out:
-		> args (*list) - unpacked list of parsed args
+def get_args(cmd_args, _description=None):
 	"""
-	from argparse import ArgumentParser
-
-	_action, _dest, _help, _required = args.keys()
-	parser = argparse.ArgumentParser(
-			action=_action,
-			dest=_dest,
-			help=_help,
-			required=_required)
+	Gets & processes some defined command line args. Should be modular, let's see how it works.
+	In:
+		> cmd_args (list) - list of lists with mandatory
+		columns "long" (name), "short" (name), "action", "dest", "help", in that order.
+		> _description (str) - optional, a description of the script this function is called in.
+	Out:
+		> args (list) - list of parsed args
+	"""
+	parser = ArgumentParser(description=_description)
+	dests = []
+	for arg in cmd_args:
+		_long, _short, _action, _dest, _help = arg
+		parser.add_argument(_long, _short,
+				action=_action,
+				dest=_dest,
+				help=_help)
+		dests.append(_dest)
 	args = parser.parse_args()
+	return [getattr(args, dest) for dest in dests]
 
-	return *[_arg for _arg in args]
+
+def get_config(config_file):
+	"""
+	Gets & processes confid from file if supplied.
+	In:
+		> config_file (str or path object) - path to config file
+	Out:
+		> cfgs (list) - a list of parsed config options
+	"""
+	cfgs = []
+	for line in open(config_file, 'r'):
+		option = line.strip().split('=')[1].strip()
+		try:
+			option = eval(option) # Do this to convert 'True' to a Bool
+		except:
+			pass
+		cfgs.append(option)
+	return cfgs
